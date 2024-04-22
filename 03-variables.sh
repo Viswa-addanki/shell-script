e0d49e9fd3f6f683aa42088f935d7389e4f414cf
Viswa_addanki <a.viswanath007@gmail.com>
2024-04-22 18:24:39 +0530

    VARIABLES

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi $PERSON2 how are u"
+echo "$PERSON2:: Hi $PERSON1 i am fine"
+echo  "$PERSON1 :: $PERSON2 can we exchange our numbers"
+
+
