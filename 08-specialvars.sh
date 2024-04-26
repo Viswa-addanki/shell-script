bfb41cc354b1c2158cb53ed5e4adc8ac65f156f4
Viswa_addanki <a.viswanath007@gmail.com>
2024-04-26 08:05:13 +0530

    some-msg

--- 08-specialvars.sh
+++ 08-specialvars.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo " all variables:$@"
+echo "numberof variables passed:$#"
+echo "script Name : $0"
+echo "current working directory:$pwd"
+echo "which user is running this script:$user"
+echo "Hostname: $HOSTNAME"
+echo "process ID of the Current Shell script: $$"
+sleep 60
+echo "process ID of last background command :$!"
+
