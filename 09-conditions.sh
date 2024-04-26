2017da5968cf6e6d706a2cdb99c564072c32166e
Viswa_addanki <a.viswanath007@gmail.com>
2024-04-26 08:19:29 +0530

    some-msg

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,7 +5,7 @@ conds=$2
 
 if [ $Number -gt $conds ]
 then
-        echo "given number $Number is greater than 10"
+        echo "given number $Number is greater than $conds"
 else
-        echo "given number $Number is less than 10"
+        echo "given number $Number is less than $conds"
 fi
