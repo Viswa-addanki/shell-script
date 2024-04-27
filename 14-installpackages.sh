cef8d5325dd657969f468b73ea55fcd4dba9c7b9
Viswa_addanki <a.viswanath007@gmail.com>
2024-04-27 12:58:11 +0530

    some-msg

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+TIMESTAMP=$(date +%F-%H-%m-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.LOG
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo "$2 ...Failure "
+    exit 1
+else
+    echo "$2...sucess"
+fi
+}
 if [$USERID -ne 0]
 
 then 
@@ -17,6 +26,12 @@ then
    for i in $@
    do 
     echo "package to install $i"
-    dnf list installed $i
+    dnf list installed $i &>>$LOGFILE
+    if [$? -ne 0]
+    then
+    echo "$i already isntalled "
+    else 
+    echo "$i not installed Need to install"
+     fi
 
     done
