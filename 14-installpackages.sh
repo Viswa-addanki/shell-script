fe2c5bb3962420a3af9371f5dddaa554fb7ec9be
Viswa_addanki <a.viswanath007@gmail.com>
2024-04-27 13:17:29 +0530

    some-msg

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%m-%S)
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.LOG
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 VALIDATE(){
-if [ $1 -ne 0 ]
-then
-    echo "$2 ...Failure "
-    exit 1
-else
-    echo "$2...sucess"
-fi
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
 }
-if [$USERID -ne 0]
 
-then 
- echo "Please run this script with root user"
- exit 1
-  else
-   echo "you are super user."
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
 
-   fi
-
-   for i in $@
-   do 
-    echo "package to install $i"
+for i in $@
+do
+    echo "package to install: $i"
     dnf list installed $i &>>$LOGFILE
-    if [$? -ne 0]
+    if [ $? -eq 0 ]
     then
-    echo "$i already isntalled "
-    else 
-    echo "$i not installed Need to install"
-     fi
-
-    done
+        echo -e "$i already installed...$Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+    fi
+done
